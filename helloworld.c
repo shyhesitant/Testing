2b9279db2927b1de6b2becd7f82ba73763a2570a
willy <WillyKGoldberg@gmail.com>
2022-09-04 14:23:54 +0300

    Hello World

---
+++ helloworld.c
@@ -0,0 +1,29 @@
+#include <ddp22_libs.h>
+#include <gpio.h>
+#include <int.h>
+
+
+int main() 
+{
+	int value = 0;
+	int dir = 0;
+	set_gpio_pin_value(1, 1); // J1 on the board, a reference to VCC
+
+	dir =  get_gpio_pin_direction(0);
+	bm_printf("\nPIN DIR IS: %d\n", dir);
+
+	set_gpio_pin_direction(0, DIR_OUT);  // DIR_OUT == 1
+	dir =  get_gpio_pin_direction(0); // Make sure the pin dir actually changed
+	bm_printf("\nPIN DIR IS: %d\n", dir);
+
+	while(1)
+	{
+		// Periodically read and print pin 0's value
+		for (volatile int i=0;i<10000;i++) {}
+		value = get_gpio_pin_value(0);
+		bm_printf("\nPIN VALUE IS: %d\n", value);
+	}
+  bm_quit_app();
+  return 0;
+}
+
